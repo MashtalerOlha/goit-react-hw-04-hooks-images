6f84fd90cde37450b6eb430a85900317cbe97660
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-04-25 14:17:34 +0300

    add hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,86 +1,72 @@
 import { api } from '../Api/Api';
-import React, { Component } from 'react';
-import Searchbar from '../Searchbar/Searchbar';
+import { useState, useEffect } from 'react';
+import { SearchForm } from '../Searchbar/Searchbar';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
 import { Spiner } from '../Loader/Loader';
 import { LoadMore } from '../Button/Button';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    imageCard: [],
-    page: 1,
-    status: 'idle',
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [imageCard, setImageCard] = useState([]);
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState('idle');
+
+  const loadMorePhoto = () => {
+    setPage(page => page + 1);
   };
 
-  LoadMorePhoto = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleFormSubmit = searchQuery => {
+    setImageCard([]);
+    setSearchQuery(searchQuery);
+    setPage(1);
   };
 
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery, page: 1 });
-  };
-
-  simpleLightbox = () => {
+  const simpleLightbox = () => {
     var lightbox = new SimpleLightbox('.gallery a', {
-      captionsData: "alt",
+      captionsData: 'alt',
     });
     lightbox.refresh();
   };
 
-  componentDidUpdate(_, prevState) {
-    this.simpleLightbox();
-    const { page, searchQuery } = this.state;
-
-    if (prevState.searchQuery !== searchQuery) {
-      this.setState({ imageCard: [] });
-    }
-
-    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
-      this.setState({ status: 'pending' });
-      api
-        .fetchImage(searchQuery, page)
-        .then(imageCard => {
-          this.setState(prevState => ({
-            imageCard: [...prevState.imageCard, ...imageCard.hits],
-            status: 'resolved',
-          }));
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
-
-    // if(this.state.status === 'resolve' || nextName.length ===0 ){
-    //   console.log('not found')
-    // }
-  }
-
-  render() {
-    const { status, imageCard } = this.state;
-
-    if (status === 'idle') {
-      return <Searchbar onSubmit={this.handleFormSubmit} />;
-    }
-
-    if (status === 'pending') {
-      return <Spiner />;
-    }
-
-    if (status === 'resolved') {
-      return (
-        <>
-          <Searchbar onSubmit={this.handleFormSubmit} />
-          <ImageGallery imageCard={imageCard} />
-          <LoadMore onClick={this.LoadMorePhoto} />
-        </>
-      );
-    }
-
-    if (status === 'rejected') {
+  useEffect(() => {
+    if (searchQuery === '') {
       return;
     }
+    setStatus('pending');
+
+    api
+      .fetchImage(searchQuery, page)
+      .then(imageCard => {
+        setImageCard(images => [...images, ...imageCard.hits]);
+
+        setStatus('resolved');
+        simpleLightbox();
+      })
+      .catch(error => setStatus('rejected'));
+  }, [page, searchQuery]);
+
+  if (status === 'idle') {
+    return <SearchForm onSubmit={handleFormSubmit} />;
   }
-}
+
+  if (status === 'pending') {
+    return <Spiner />;
+  }
+
+  if (status === 'resolved') {
+    return (
+      <>
+        <SearchForm onSubmit={handleFormSubmit} />
+        <ImageGallery imageCard={imageCard} />
+        <LoadMore onClick={loadMorePhoto} />
+      </>
+    );
+  }
+
+  if (status === 'rejected') {
+    return;
+  }
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,50 +1,46 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import s from './Searchbar.module.css';
 import { FaSearch } from 'react-icons/fa';
 
-export default class SearchForm extends Component {
-  state = {
-    imageCard: [],
-    searchName: '',
+export const SearchForm = ({ onSubmit }) => {
+  const [searchName, setSearchName] = useState('');
+
+  const handleNameSearch = e => {
+    setSearchName(e.currentTarget.value.toLowerCase());
   };
 
-  handleNameSearch = e => {
-    this.setState({ searchName: e.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.searchName.trim() === '') {
+    if (searchName.trim() === '') {
       return toast.error('Enter a search query');
     }
 
-    this.props.onSubmit(this.state.searchName);
+    onSubmit(searchName);
+    setSearchName('');
   };
 
-  render() {
-    return (
-      <header className={s.Searchbar}>
-        <Toaster />
-        <form className={s.SearchForm} onSubmit={this.handleSubmit}>
-          <button className={s.SearchFormButton} type="submit">
-            <span className={s.SearchFormButtonLabel}>Search</span>
-          </button>
-          <FaSearch className={s.svg} />
+  return (
+    <header className={s.Searchbar}>
+      <Toaster />
+      <form className={s.SearchForm} onSubmit={handleSubmit}>
+        <button className={s.SearchFormButton} type="submit">
+          <span className={s.SearchFormButtonLabel}>Search</span>
+        </button>
+        <FaSearch className={s.svg} />
 
-          <input
-            className={s.SearchFormInput}
-            name="searchName"
-            value={this.state.searchName}
-            onChange={this.handleNameSearch}
-            type="text"
-            autoFocus
-            autoComplete="off"
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={s.SearchFormInput}
+          name="searchName"
+          value={searchName}
+          onChange={handleNameSearch}
+          type="text"
+          autoFocus
+          autoComplete="off"
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
